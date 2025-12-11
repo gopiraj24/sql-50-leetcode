3322727d9509d87c7e3c2d4880bb0dd412a40f3e
Gopi <74865665+gopiraj24@users.noreply.github.com>
2025-12-11 23:19:29 +0530

    Time: 5 ms (96.96%) | Memory: 44.1 MB (50.09%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        for(char c: s1.toCharArray()) {
+            s1Count[c - 'a']++;
+        }
+
+        int window = s1.length();
+        
+        for(int i = 0; i<s2.length();i++) {
+         s2Count[s2.charAt(i) - 'a']++;
+
+         if (i >= window) {
+            s2Count[s2.charAt(i - window) - 'a']--;
+         }
+
+
+         if(i>=window-1) {
+            if(Arrays.equals(s1Count,s2Count)) {
+                return true;
+            }
+         }
+        }
+        return false;
+    }
+}
