3235247896e581fc1a3882973e4c3270e2220895
Gopi <74865665+gopiraj24@users.noreply.github.com>
2025-12-14 22:52:56 +0530

    Time: 1 ms (93.46%) | Memory: 42.9 MB (14.11%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int pointer1 = 0;
         int pointer2 = 0;
-        int k = s.length()-1;
-        int r = t.length()-1;
+        int k = s.length();
+        int r = t.length();
         
 
-        while(pointer1 <= k && pointer2 <= r ) {
+        while(pointer1 < k && pointer2 <r ) {
             char c = t.charAt(pointer2);
             if(s.charAt(pointer1) == c ) {
                 pointer1++;
